73e88967e15f843c1442be38225c11972d79338e
Patrick Passarella <patrick.passarella@BERTON.local>
2018-02-16 18:06:52 -0200

    Added in progress code

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h3>Word Translator</h3>
+    <h5>Powered By Vue.js</h5>
+    <TranslateForm v-on:formSubmit="translateText"></TranslateForm>
+  </div>
+</template>
+
+<script>
+
+import TranslateForm from './components/translateForm'
+
+export default {
+  name: 'App',
+  components: {
+    'TranslateForm':TranslateForm
+  },
+  methods: {
+    translateText: function (text) {
+      this.$htto.get('yandex url');
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/translateForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="translateForm">
+
+      <form v-on:submit="formSubmit">
+          <input type="text" v-model="textToTranslate" placeholder="Enter a word to translate">
+          <input type="submit" value="Translate">
+      </form>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'translateForm',
+  data () {
+      return {
+          textToTranslate: ''
+      }
+  },
+  methods: {
+      formSubmit (e) {
+          e.preventDefault();
+          this.$emit('formSubmit', this.textToTranslate)
+      }
+  }
+}
+</script>
+
+<style>
+
+</style>
